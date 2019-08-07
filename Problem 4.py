96c2e97782ebd3ea72063431b8a8711b7758a427
saumonarticho <stagriko@gmail.com>
2019-08-07 22:32:35 +0200

    Delete Problem 4.py

--- Problem 4.py
+++
@@ -1,6 +0,0 @@
-def Palindrome():
-    for x in range(100,1000):
-        for y in range(x,1000):
-            string = str(x*y)
-            if string[::-1] == string:
-                print(string)
